437269bd1ca60474f675df4698b43562f6566e37
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-13 13:15:16 +0100

    Fix formatting in part of the day enum

--- Assets/Core/Code/DayNightSystem/Scripts/PartOfDay.cs
+++ Assets/Core/Code/DayNightSystem/Scripts/PartOfDay.cs
@@ -2,11 +2,11 @@ namespace DayNightSystem.Scripts
 {
     public enum PartOfDay
     {
-        Dawn=5, 
+        Dawn = 5,
         Morning=7, 
         Noon=12, 
         Afternoon=17, 
-        Dusk=18, 
-        Night=20
+        Dusk=18,
+        Night = 20
     }
 }
