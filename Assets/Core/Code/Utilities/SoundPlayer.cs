8656aaf25c791b5342a9bbcb82eed8bf39477110
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-13 15:57:32 +0100

    Add play on start option ot sound player

--- Assets/Core/Code/Utilities/SoundPlayer.cs
+++ Assets/Core/Code/Utilities/SoundPlayer.cs
@@ -7,6 +7,7 @@ namespace Utilities
     {
         [SerializeField] private AudioClip audioClip;
         [SerializeField] private bool loop = false;
+        [SerializeField] private bool playOnStart = false;
         private AudioSource audioSource;
 
         private void Start()
@@ -14,6 +15,7 @@ namespace Utilities
             audioSource = GetComponent<AudioSource>();
             audioSource.clip = audioClip;
             audioSource.loop = loop;
+            if (playOnStart) PlaySound();
         }
 
         public void PlaySound()
