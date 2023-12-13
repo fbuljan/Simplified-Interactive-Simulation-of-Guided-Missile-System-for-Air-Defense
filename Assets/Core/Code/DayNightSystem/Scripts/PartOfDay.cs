5614a60d37ed00426f45641fd2b626a740264849
Filip Buljan <filipbuljan2002@gmail.com>
2023-12-13 13:15:41 +0100

    Further formatting fix

--- Assets/Core/Code/DayNightSystem/Scripts/PartOfDay.cs
+++ Assets/Core/Code/DayNightSystem/Scripts/PartOfDay.cs
@@ -3,10 +3,10 @@ namespace DayNightSystem.Scripts
     public enum PartOfDay
     {
         Dawn = 5,
-        Morning=7, 
-        Noon=12, 
-        Afternoon=17, 
-        Dusk=18,
+        Morning = 7,
+        Noon = 12,
+        Afternoon = 17,
+        Dusk = 18,
         Night = 20
     }
 }
