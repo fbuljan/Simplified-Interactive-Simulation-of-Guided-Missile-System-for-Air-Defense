d6c68ed53d6ec83a7150e60f735ba05db43755ce
Filip Buljan <filipbuljan2002@gmail.com>
2024-01-17 19:08:22 +0100

    Implement random explosions during simulation

---
+++ Assets/Core/Code/Simulations/ExplosionsSimulator.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Simulations
+{
+    public class ExplosionsSimulator : MonoBehaviour
+    {
+        [SerializeField] private GameObject explosionPrefab;
+        [SerializeField] private SimulationController simulationController;
+        [SerializeField, Min(3f)] private float maxTimeBetweenExplosions = 15f;
+        private Terrain terrain;
+
+        private void OnEnable()
+        {
+            simulationController.OnSimulationMilestone += OnSimulationMilestone;
+        }
+
+        private void OnDisable()
+        {
+            simulationController.OnSimulationMilestone -= OnSimulationMilestone;
+        }
+
+        private void OnSimulationMilestone(bool obj)
+        {
+            if (obj)
+            {
+                Invoke(nameof(SpawnRandomExplosion), Random.Range(1f, maxTimeBetweenExplosions));
+                return;
+            }
+
+            CancelInvoke(nameof(SpawnRandomExplosion));
+        }
+
+        private void Start()
+        {
+            terrain = Terrain.activeTerrain;
+        }
+
+        private void SpawnRandomExplosion()
+        {
+            Vector3 randomPosition = new Vector3(Random.Range(0, terrain.terrainData.size.x), 0, Random.Range(0, Terrain.activeTerrain.terrainData.size.z)) + Terrain.activeTerrain.transform.position;
+            randomPosition.y = Terrain.activeTerrain.SampleHeight(randomPosition) + Terrain.activeTerrain.transform.position.y;
+            Instantiate(explosionPrefab, randomPosition, Quaternion.identity);
+
+            Invoke(nameof(SpawnRandomExplosion), Random.Range(1f, maxTimeBetweenExplosions));
+        }
+    }
+}
